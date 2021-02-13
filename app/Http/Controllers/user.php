deb7a188ad48ddf6393e6f7d71cebc7d76e63c56
TheEsha <theunsung_256@yahoo.com>
2021-02-13 23:52:57 +0500

    user's API

---
+++ app/Http/Controllers/user.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\userModel;
+use Illuminate\Http\Request;
+use Validator;
+
+
+class users extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(userModel::get(),200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = userModel::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = userModel::find($id);
+        if (is_null($user)) {
+            return response()->json(['message'=>'Record Not Found!'],404);
+        }
+         return response()->json($user, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = userModel::find($id);
+        if (is_null($user)) {
+            return response()->json(['message'=>'Record Not Found!'],404);
+        }
+        $user->update($request->all());
+        return response()->json($user,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = userModel::find($id);
+        if (is_null($user)) {
+            return response()->json(['message'=>'Record Not Found!'], 404);
+        }
+        $user->delete();
+      return response()->json(null,204);
+
+
+    }
+}
